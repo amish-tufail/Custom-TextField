826f10603cef5e209b022ce1539246072375f902
Amish <amish.tufail2002@gmail.com>
2024-02-03 18:05:29 +0500

    - Created TextField - Implemented

--- CustomTextField/ContentView.swift
+++ CustomTextField/ContentView.swift
@@ -8,12 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var text: String = ""
+    @State  var isEditing: Bool = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextFieldAnimation(text: $text, title: "Email", isEditing: $isEditing)
         }
         .padding()
     }
@@ -21,4 +21,38 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct TextFieldAnimation: View {
+    @Binding var text: String
+    var title: String
+    @Binding var isEditing: Bool
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0.0) {
+            ZStack(alignment: .leading) {
+                RoundedRectangle(cornerRadius: 8.0, style: .continuous)
+                    .stroke(.black, lineWidth: 1.0)
+                    .frame(height: 40.0)
+                Text(title)
+                    .font(.system(size: isEditing ? 10.0 : 15.0))
+                  
+                    .padding(10.0)
+                    .zIndex(isEditing ? 2.0 : 0.0)
+                    .background(
+                        RoundedRectangle(cornerRadius: 12.0)
+                            .fill(isEditing ? .white : .clear)
+                            .opacity(isEditing ? 1.0 : 0.0)
+                            .frame(width: 30.0, height: 10)
+                    )
+                    .offset(y: isEditing ? -20.0 : 0.0)
+                TextField("", text: $text) { editing in
+                    withAnimation {
+                        isEditing = editing || (!text.isEmpty) // when not empty || (text.isEmpty != true)
+                    }
+                }
+                .padding(5.0)
+            }
+        }
+        
+    }
 }
