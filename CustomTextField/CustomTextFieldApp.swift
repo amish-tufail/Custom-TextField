db9f5bae105cdf227e503387dc5449091d51bff8
Amish <amish.tufail2002@gmail.com>
2024-02-03 17:38:51 +0500

    Initial Commit

---
+++ CustomTextField/CustomTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTextFieldApp.swift
+//  CustomTextField
+//
+//  Created by Amish Tufail on 03/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CustomTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
